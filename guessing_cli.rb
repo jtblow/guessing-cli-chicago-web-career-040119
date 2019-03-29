d3bd563107395ee11aef34409320c7670d22671d
Jake Bartlow <jtbartlow@gmail.com>
2019-03-29 21:24:50 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,2 +1,8 @@
 # Code your solution here!
 def run_guessing_game
+  user_input = ""
+  rando = nil 
+  until user_input = rando
+    puts "Please guess a number"
+    user_input = gets.chomp.strip
+    
