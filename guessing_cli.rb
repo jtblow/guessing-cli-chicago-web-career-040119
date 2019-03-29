287deabd6d2e00237ba4d60c04561a16d7d56cb6
Jake Bartlow <jtbartlow@gmail.com>
2019-03-29 21:33:50 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   user_input = ""
   rando = nil 
   until user_input == "exit"
-    puts "Guess a number between 1 and 6"
+    puts "Guess a number between 1 and 6."
     user_input = gets.chomp
     rando = rand(1..6)
     if user_input == rando
