cd19aa270910b1162755e22874af9c282abcb77a
Jake Bartlow <jtbartlow@gmail.com>
2019-03-29 21:29:56 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   user_input = ""
   rando = nil 
-  until user_input = rando
+  until user_input == rando
     puts "Please guess a number"
     user_input = gets.chomp
     rando = rand(1..10)
