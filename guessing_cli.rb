280a4e2bc2097614ed8f78baad1d05cbc77209c2
Jake Bartlow <jtbartlow@gmail.com>
2019-03-29 21:32:25 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,9 +2,16 @@
 def run_guessing_game
   user_input = ""
   rando = nil 
-  until user_input == rando
+  until user_input == "exit"
     puts "Please guess a number"
     user_input = gets.chomp
     rando = rand(1..10)
     if user_input == rando
+      puts "You guessed the correct number!"
+    else 
+      puts "The computer guessed #{rando}."
+    end
+    puts "Goodbye"
+  end
+     
     
